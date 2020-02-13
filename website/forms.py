0f7746abd64ab3033b5006d03848d9778edca92a
Hữu Tuấn Anh <huutuananh1406@gmail.com>
2020-02-13 16:36:05 +0700

    Cập nhật giao diện sửa bài viết

--- website/forms.py
+++ website/forms.py
@@ -57,3 +57,37 @@ class FormBaiViet(forms.Form):
             for sanpham in dssanpham
             ]
     sanpham = forms.ChoiceField(label="Sản phẩm", choices=dsluachon)
+
+class FormSuaSanPham(forms.ModelForm):
+    class Meta:
+        model = Sanpham
+        fields = [
+                'sanpham_ten',
+                'sanpham_hinhanh',
+                'sanpham_giatien',
+                ]
+
+class FormSuaBaiViet(forms.ModelForm):
+    baiviet_noidung = forms.CharField(
+            label="Nội dung",
+            widget=CKEditorUploadingWidget,
+            )
+    baiviet_noidung_rutgon = forms.CharField(
+            label="Nội dung rút gọn",
+            widget=CKEditorUploadingWidget,
+            )
+    dssanpham = Sanpham.objects.all()
+    dsluachon = [
+            (sanpham.sanpham_id, sanpham.sanpham_ten)
+            for sanpham in dssanpham
+            ]
+    baiviet_sanpham_id = forms.ChoiceField(label="Sản phẩm", choices=dsluachon)
+    class Meta:
+        model = BaiViet
+        fields = [
+                'baiviet_tieude',
+                'baiviet_hinhanh',
+                'baiviet_noidung',
+                'baiviet_noidung_rutgon',
+                'baiviet_sanpham_id',
+                ]
